c73836da34e1702d86c43498974a1310a1d853dc
Iseg6ix <freddyjseg@gmail.com>
2022-09-09 10:35:59 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *Everything is done in the main function
- *It returns a value of zero if no errors is encountered, else it returns a non zero value
+ *main - Serves as the entry point of the code. Everything is done in the main function.
+ *Return: It returns a value of zero if no errors is encountered, else it returns a non zero value.
 */
 
-int main(){
-	puts("Programming is like building a multilingual puzzle");
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
