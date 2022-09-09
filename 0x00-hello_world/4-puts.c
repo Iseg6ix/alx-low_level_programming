f8d9ad5c4b9b9959203127a7c3de115875b42b4b
Iseg6ix <freddyjseg@gmail.com>
2022-09-09 10:38:07 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- *main - Serves as the entry point of the code. Everything is done in the main function.
+ *main - Serves as the entry point of the code.
  *Return: It returns a value of zero if no errors is encountered, else it returns a non zero value.
 */
-
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
