0fb40e1d9863f3950fef97b099beb7be47beef2d
Iseg6ix <freddyjseg@gmail.com>
2022-09-09 10:31:55 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ *Everything is done in the main function
+ *It returns a value of zero if no errors is encountered, else it returns a non zero value
+*/
 
 int main(){
 	puts("Programming is like building a multilingual puzzle");
