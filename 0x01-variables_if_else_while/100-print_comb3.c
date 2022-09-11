0d7837abe47f3b60fe14348af4575c5a094b6977
Iseg6ix <freddyjseg@gmail.com>
2022-09-11 13:23:01 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,13 +12,14 @@ int main(void)
 	for (a = '0'; a <= '9'; a++)
 	{
 		for (b = '1'; b <= '9'; b++)
-			if (a != '9')
-		{
+			if (a != '9' && a != b)
+
+			{
 				putchar(a);
 				putchar(b);
 				putchar(',');
 				putchar(' ');
-		};
+			};
 	};
 	putchar('\n');
 	return (0);
