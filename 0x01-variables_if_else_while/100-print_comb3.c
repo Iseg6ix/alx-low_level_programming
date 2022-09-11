1b834ca9edc17992183880e2c7ea9d19e5aa3bd3
Iseg6ix <freddyjseg@gmail.com>
2022-09-11 12:28:44 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main- This program prints out all possible
+ * combinations of 2 unidentical numbers.
+ * Return: 0 if no errors occurs else a +ve integer.
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '1'; b <= '9'; b++)
+			if (a != '9')
+		{
+				putchar(a);
+				putchar(b);
+				putchar(',');
+				putchar(' ');
+		};
+	};
+	putchar('\n');
+	return (0);
+}
