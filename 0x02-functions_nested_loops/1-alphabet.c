ce90772c48065d494278da32b71a7f53e8acb9ed
Iseg6ix <freddyjseg@gmail.com>
2022-09-13 09:31:47 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,13 @@
  * main - uses void print_alphabet to print out the alphabets
  * twice in 2 lines.
  *
- * Return: 0 on success.
+ * Return: Void on success.
  */
-int print_alphabet(void)
+void print_alphabet(void)
 {
 	int a;
 
 	for (a = "a", a <= "z" a++)
 		_putchar(a);
 	_putchar('\n');
-	return (0);
 }
