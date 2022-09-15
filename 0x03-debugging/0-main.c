e08b5071da3d894f459de23bb20b824ab5a2ce11
Freedom Iziegbe Iseghohimhen <freddyjseg@gmail.com>
2022-09-15 12:38:30 +0100

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or not
- * Return: 0 on success.
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  **/
 
 int main(void)
