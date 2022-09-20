10f08e9323cc6e4d81426e43fe7d2f27af02954c
Iseg6ix <freddyjseg@gmail.com>
2022-09-19 19:27:33 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * rev_string - This function prints a strin in reverse.
+ * @s: Pointer argument of the function.
+ *
+ * Return: Nothing.
+ */
+void rev_String(char *s)
+{
+	int i = strlen(s);
+
+	while (i--)
+	{
+		putchar(*(s + i));
+	}
+	putchar('\n');
+}
