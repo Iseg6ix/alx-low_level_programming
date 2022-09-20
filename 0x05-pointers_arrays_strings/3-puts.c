0651c2f1510576a1e8c054a84a694617868ad00f
Iseg6ix <freddyjseg@gmail.com>
2022-09-19 17:53:05 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _puts - This function prints a string, followed by a new line, to stdout.
+ * @str - Pointer argument of the function.
+ *
+ * Return: Nothing.
+ */
+void _puts(char *str)
+{
+	puts("%x\n", *str);
+}
