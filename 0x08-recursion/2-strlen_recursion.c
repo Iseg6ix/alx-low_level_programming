44d53568299a6a6c3c9aa87b02e878bd737c07d0
Freedom Iziegbe Iseghohimhen <freddyjseg@gmail.com>
2022-09-30 00:34:44 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,35 +1,14 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * _strlen_recursion - This function prints out the length of a string.
- * @s: The pointer argument of the function.
- * 
- * Return: 0 on success.
- */
-
-#include "main.h"
-
-/** _strlen_recursion - The function prints out a string in reverse
- * *s: string variable
- * Return: *s
+ * _strlen_recursion - returns the length of a string.
+ * @s: string to check
+ * Return: the length of a string.
  */
 int _strlen_recursion(char *s)
 {
-        int i = 1;
-
-        if (*(s + 1) == '\0')
-                return 0;
-        else
-        {
-                i += _strlen_recursion(s + 1);
-                i++;
-        
-        }
-        return i;
-}
-int main(void)
-{
-    int a = _strlen_recursion("bad");
-    printf("%d", a);
-    return 0;
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
