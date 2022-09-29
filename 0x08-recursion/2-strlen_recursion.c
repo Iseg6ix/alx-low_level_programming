eb783a65d39676cc2971e0467e24d5e23e7ec5b3
Freedom Iseghohimhen <freddyjseg@gmail.com>
2022-09-29 23:11:45 +0100

    Task 3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * _strlen_recursion - This function prints out the length of a string.
@@ -5,14 +6,30 @@
  * 
  * Return: 0 on success.
  */
+
+#include "main.h"
+
+/** _strlen_recursion - The function prints out a string in reverse
+ * *s: string variable
+ * Return: *s
+ */
 int _strlen_recursion(char *s)
 {
-    	int i = 0;
+        int i = 1;
 
-	if (*s != '\0')
-	{
-		i++;
-		i += _strlen_recursion(s + 1);
-	}
-	return (i);
+        if (*(s + 1) == '\0')
+                return 0;
+        else
+        {
+                i += _strlen_recursion(s + 1);
+                i++;
+        
+        }
+        return i;
+}
+int main(void)
+{
+    int a = _strlen_recursion("bad");
+    printf("%d", a);
+    return 0;
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
 int _putchar(char c);
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
