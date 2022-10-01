163f213edb886f2c287de14514d18021524dffac
Iseg6ix <freddyjseg@gmail.com>
2022-09-30 17:02:31 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - This function returns the name of the compiled file.
+ * @argc: Count of the number of arguments entering the function.
+ * @argv: Array of the arguments entering the function.
+ *
+ * Return: 0 On success.
+ */
+int main(int argc, char **argv)
+{
+	_putchar("%s\n", argv[0]);
+	return (0);
+}
