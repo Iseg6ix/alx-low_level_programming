a524bc073d39dd189648b14334bc4a4d8775f86e
Iseg6ix <freddyjseg@gmail.com>
2022-09-30 17:38:10 -0700

    Task 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - This function prints the number of arguments passed.
+ * @argc: Integer argument.
+ * @argv: Pointer argument pointing to a character.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char* argv[] __attribute__((unused)))
+{
+	_putchar("%d", argc);
+	_putchar("\n");
+	return (0);
+}
