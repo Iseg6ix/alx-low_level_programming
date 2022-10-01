896331b9a5c3a463d6bc6c00accf4e9c48ce175f
Iseg6ix <freddyjseg@gmail.com>
2022-09-30 17:46:13 -0700

    T

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	_putchar("%s", *argv[0]);
+	_putchar("%s", *argv);
 	_putchar("\n");
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,2 +1,1 @@
-#include <stdio.h>
 _putchar(char n);
