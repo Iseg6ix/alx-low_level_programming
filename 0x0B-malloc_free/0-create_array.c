85f3eee1f3ebecff0966f9977430458b451b2975
Iseg6ix <freddyjseg@gmail.com>
2022-10-05 16:01:00 -0700

    Task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/**
+ * create_array - This function does something.
+ * :c - Argument
+ *
+ * Return: 0 if size is zero
+ */
+char *create_array(unsigned int size, char c)
+{
+	char a = [sizeof(c)];
+
+
