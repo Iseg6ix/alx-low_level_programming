413f66a1508706dc91b276af34c6e07e13199732
Iseg6ix <freddyjseg@gmail.com>
2022-10-06 18:01:57 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * malloc_checked - This function doess something.
+ * @b: First argument.
+ *
+ * Return: NULL.
+ */
+void *malloc_checked(unsigned int b);
+{
+	int *p;
+
+	*p = malloc(sizeof(b));
+	if (p = NULL)
+		return (exit(98));
+	return *p;
+}
