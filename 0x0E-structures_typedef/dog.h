b9316c97af7572deb3095868c98b6fa502770a8f
Iseg6ix <freddyjseg@gmail.com>
2022-10-11 23:31:25 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * struct dog - structure for dogs
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: pet's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
