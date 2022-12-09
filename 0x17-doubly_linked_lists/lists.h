64fc6faa79cacd64a97ac516f235cf61eda6cf40
Freedom Iziegbe Iseghohimhen <freddyjseg@gmail.com>
2022-12-09 18:14:47 +0100

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,22 +1,25 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
  * @prev: points to the previous node
- * @node: points to the next node
+ * @next: points to the next node
  *
  * Description: doubly linked list node structure
+ * 
  */
 typedef struct dlistint_s
 {
-	int data;
-	struct dlistint_s *next;
-	struct dlistint_s *prev;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
+
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
